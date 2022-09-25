da5795baa0f35913a9c36ff42f7c86bb1b3e1db0
1052538783 <98883700+1052538783@users.noreply.github.com>
2022-09-26 00:18:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oMqsy6do7Y3DCzw0vNgR4q0HDpfE',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '	cVRaXs99mZ4VHI4IPIvBGID5FzleZpJ9CY_9ZTcbwfk',
+      useTemplateId: 'cVRaXs99mZ4VHI4IPIvBGID5FzleZpJ9CY_9ZTcbwfk',
       // 所在省份
       province: '广东',
       // 所在城市
